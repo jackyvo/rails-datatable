5925930bc7d108a2e3fab69ef83a1d059380ea4a
Jacky <jackyinvn@gmail.com>
2022-09-14 11:08:03 +0700

    Init gem

---
+++ lib/rails/datatable.rb
@@ -0,0 +1,8 @@
+require "rails/datatable/version"
+
+module Rails
+  module Datatable
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/rails/datatable_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Rails::Datatable do
+  it "has a version number" do
+    expect(Rails::Datatable::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
